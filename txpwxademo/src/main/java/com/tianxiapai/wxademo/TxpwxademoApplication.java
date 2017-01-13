01a5c5740104e19dbdc8f2931825a04b9ebe1cad
Pocketpc <Pocketpc@192.168.200.192>
2017-01-13 10:47:04 +0800

    init

---
+++ txpwxademo/src/main/java/com/tianxiapai/wxademo/TxpwxademoApplication.java
@@ -0,0 +1,12 @@
+package com.tianxiapai.wxademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxpwxademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxpwxademoApplication.class, args);
+	}
+}
---
+++ txpwxademo/src/test/java/com/tianxiapai/wxademo/TxpwxademoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tianxiapai.wxademo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TxpwxademoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
