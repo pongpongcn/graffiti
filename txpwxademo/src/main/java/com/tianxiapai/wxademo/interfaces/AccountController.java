3ed942b8d74cdf34b184ddd6fcc9659b4232550e
Pocketpc <Pocketpc@192.168.200.192>
2017-01-17 16:26:08 +0800

    Enable security.

--- txpwxademo/src/main/java/com/tianxiapai/wxademo/interfaces/AccountController.java
+++ txpwxademo/src/main/java/com/tianxiapai/wxademo/interfaces/AccountController.java
@@ -1,12 +1,21 @@
 package com.tianxiapai.wxademo.interfaces;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tianxiapai.wxademo.exception.WxaDemoErrorException;
@@ -27,6 +36,13 @@ public class AccountController {
 		this.wxService = wxService;
 	}
 	
+	@RequestMapping(value = "/ping", method = RequestMethod.GET)
+	public Map<String, String> ping(HttpSession session){
+		Map<String,String> sa = new HashMap<>();
+		sa.put("sessionId", session.getId());
+		return sa;
+	}
+	
 	@RequestMapping(value = "/login_wx", method = RequestMethod.POST)
 	public void login(@RequestParam String code, HttpSession session) throws WxaDemoErrorException {
 		if (logger.isDebugEnabled()) {
@@ -36,6 +52,9 @@ public class AccountController {
 		try {
 			WxSession wxSession = wxService.getSessionByJsCode(code);
 			session.setAttribute("wxSession", wxSession);
+			
+			Authentication authentication = new UsernamePasswordAuthenticationToken("WeixinUser_" + code, null, AuthorityUtils.NO_AUTHORITIES);
+	        SecurityContextHolder.getContext().setAuthentication(authentication);
 		} catch (WxErrorException e) {
 			if (logger.isWarnEnabled()) {
 				logger.warn("Execute getSessionByJsCode failed, js code: {}.", code);
@@ -43,6 +62,16 @@ public class AccountController {
 
 			WxaDemoError error = new WxaDemoError(345, "Get Session failed.");
 			throw new WxaDemoErrorException(error, e);
+		}
+	}
+	
+	@RequestMapping("/logout")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void logout(HttpSession session) {
+		session.invalidate();
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("Session has been invalidate, session id: {}.", session.getId());
 		}
 	}
 }
